02ef69ecb89ea1217f25079a2afbc95da8024c67
Robert Nester <robertnester@ATM0596.lan>
2024-02-06 23:27:35 +0100

    feat(add-task-module): provide a design for the main page, add icons

---
+++ src/components/Icons/CheckMark.tsx
@@ -0,0 +1,7 @@
+export const CheckMarkSVG = () => {
+    return (
+        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 5.83333L7.99996 15.8333L3.41663 11.25L4.59163 10.075L7.99996 13.475L16.825 4.65833L18 5.83333Z" fill="white" />
+        </svg>
+    )
+};
